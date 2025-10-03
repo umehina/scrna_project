061cc25c63dd0e5513a2264b40ddc060cdd2e5fe
hinata <16894767+umehina@users.noreply.github.com>
2025-10-03 15:53:56 -0400

    add workflow and remove unnecessary packages
    vania test
    
    fixed test
    
    first branch change
    
    chore: test git
    
    chore: creating a test branch

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
+	fmt.Println("Vania's first branch")
+	fmt.Println("Hello world!")
+	fmt.Println("Hello Go!")
+
+	fmt.Println("Hello test branch.")
 
 }
