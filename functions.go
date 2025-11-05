2e0f068320ff303d7c6ba28db70fa516581e935e
amyji2003-hub <amyji@andrew.cmu.edu>
2025-11-05 16:22:52 -0500

    feat: added cmToDense function to convert CountMatrix to mat.Dense object

--- functions.go
+++ functions.go
@@ -1,3 +1,31 @@
 // functions.go
 
 package main
+
+import (
+	//"gonum.org/v1/gonum/floats"
+	"gonum.org/v1/gonum/mat"
+)
+
+// Amy Ji - 11/05/2025
+// cmToDense takes as input a countMatrix and convert it into mat.Dense object usable by the gonum module (i.e. pca).
+func cmToDense(cm *CountMatrix) *mat.Dense {
+	// findAllGenes is a countMatrix attribute (we defined this in preprocessing.go)
+	genes := cm.findAllGenes() // this returns a []string, in which each string is a gene name. 
+
+	nCells := len(cm.cells)
+	nGenes := len(genes)
+
+	//mat.NewDense is a function in Gonum/mat.
+	data := mat.NewDense(nCells,nGenes,nil)
+
+	// Fill the matrix
+	for i,cell := range cm.cells {
+		for j,g := range genes {
+			val := cell.features[g]
+			// data.Set is a function in Gonum/mat
+			data.Set(i,j,val)
+		}
+	}
+	return data
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module umemiya.me/cmu/02601/scango
 
 go 1.25.0
 
-require github.com/sjwhitworth/golearn v0.0.0-20221228163002-74ae077eafb2 // indirect
+require (
+	github.com/sjwhitworth/golearn v0.0.0-20221228163002-74ae077eafb2 // indirect
+	gonum.org/v1/gonum v0.16.0 // indirect
+)
